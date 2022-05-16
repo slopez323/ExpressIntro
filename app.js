6c88f1ddd6cedae5414e91a2dd61a7890e28508b
Sarah Lopez <slopez323@gmail.com>
2022-05-16 14:45:21 -0400

    status codes

--- app.js
+++ app.js
@@ -3,9 +3,21 @@ const app = express()
 const port = 4000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Sarah')
+})
+
+app.get('/error301', (req, res) => {
+    res.status(301).send('Error 301')
+})
+
+app.get('/error409', (req, res) => {
+    res.status(409).send('Error 409')
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('Error 404: Route not found')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
